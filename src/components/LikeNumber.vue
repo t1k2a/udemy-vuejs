e5ded9ff01ea416ced03cb2be76cb99cc52c8e51
t1k2a <maxtj1122@gmail.com>
2022-02-07 10:39:54 +0900

    セクション８まで

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div>
+        <p>いいね({{ halfNumber }})</p>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props:["totalNumber"],
+  computed: {
+    halfNumber() {
+      return this.totalNumber / 2
+    }
+  },
+  data() {
+    return {
+      number: 5
+    };
+  },
+  methods: {
+    increment() {
+      this.$emit("my-click", this.totalNumber + 1); // 第１引数はケバブケース
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
