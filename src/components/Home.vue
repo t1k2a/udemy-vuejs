141fcb0cef6a1da27160de810f59ae99876d2b08
t1k2a <maxtj1122@gmail.com>
2022-03-04 21:56:19 +0900

    セクション１０まで

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,18 @@
 <template>
-    <p>Home</p>
+    <p v-border:solid.shadow="{width: '6px', color: 'brown'}">Home</p>
 </template>
+
+<script>
+export default {
+    directives : {
+        border(el, binding) {
+            el.style.borderWidth = binding.value.width;
+            el.style.borderColor = binding.value.color;
+            el.style.borderStyle = binding.arg;
+            if (binding.modifiers.shadow) {
+                el.style.boxShadow = "0 2px 5px"
+            }
+        }
+    }
+}
+</script>
